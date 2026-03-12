aa5613e2cd3142af62099449d834043b1dbaa6c9
Darev <dmitry.gr.web@gmail.com>
2026-03-12 22:18:26 +0200

    fix DEFAULT_REMOVE_AFTER_SEC

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -7,5 +7,5 @@ export const REMOVE_LOST_AFTER_MS = 20_000
 export const DEFAULT_COUNT = 100
 export const MAX_COUNT = 200
 
-export const DEFAULT_REMOVE_AFTER_SEC = 300
+export const DEFAULT_REMOVE_AFTER_SEC = 20
 export const MAX_REMOVE_AFTER_SEC = 300
